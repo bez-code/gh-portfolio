34418c68021a8ef155d8f406877bfcda90e53311
bez <Behzad.p.ashrafi@gmail.com>
2023-12-19 21:06:04 +0300

    github service

--- gh-portfolio/src/app/app.component.ts
+++ gh-portfolio/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+ constructor( private githubService : GithubService){}
+
+
   title = 'gh-portfolio';
-  username = 'bez-code'
+  username = ''
+
+
+  ngOnInit(): void {
+    this.username = this.githubService.userName;
+  }
+
+
 }
